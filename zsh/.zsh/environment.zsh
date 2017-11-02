6055f2b42efd717c3f7979a8245ffeca07e09db8
xorana <7677151+xorana@users.noreply.github.com>
2017-11-02 14:59:26 +1100

    effectively got rid of .lesshst

--- zsh/.zsh/environment.zsh
+++ zsh/.zsh/environment.zsh
@@ -9,3 +9,7 @@ export PATH=/usr/local/sbin:$PATH
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/.local/sbin:$PATH
 export PATH=$HOME/bin:$PATH
+
+# .lesshst
+export LESSHISTSIZE=0
+export LESSHISTFILE=/dev/null
