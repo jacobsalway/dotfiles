4de9d1e8e3d1383d3fc863beb6709ad6279a1f24
xorana <7677151+xorana@users.noreply.github.com>
2017-11-02 12:01:18 +1100

    rewrite aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,27 +1,38 @@
 export EDITOR=vim
 
-alias ls='ls --color=auto'
-alias mls='ls -C -F -h --color=always'
-alias l='mls -l'
-alias ll='mls -l'
-alias la='mls -lA'
+alias cl='clear'
 
-alias g='git'
-alias ka='killall'
+alias aedit='$EDITOR $ZSH_CONFIG/aliases.zsh'
 
-alias cd=' cd'
-alias ..=' cd ..; ls'
-alias ...=' cd ..; cd ..; ls'
-alias ....=' cd ..; cd ..; cd ..; ls'
-alias cd..='..'
-alias cd...='...'
-alias cd....='....'
-alias cs='cd;ls'
+# git
+alias ga='git add'
+alias gp='git push'
+alias gl='git log'
+alias gs='git status'
+alias gd='git diff'
+alias gm='git commit -m'
+alias gma='git commit -am'
+alias gb='git branch'
+alias gc='git checkout'
+alias gra='git remote add'
+alias grr='git remote rm'
+alias gpu='git pull'
+alias gcl='git clone'
+alias gta='git tag -a -m'
+alias gf='git reflog'
 
-alias /='cd /'
 alias ~='cd ~'
+alias /='cd /'
+
+alias ls='ls -GFh --color=auto'
+alias ll='ls -GFhl --color=auto'
+alias lsa='ls -GFha --color=auto'
+alias lla='ls -GFhla --color=auto'
+
+alias ..='cd ..; ls'
+alias ...='cd ../..; ls'
+alias ....='cd ../../..; ls'
+alias .....='cd ../../../..; ls'
 
 alias md='mkdir -p'
 alias rd='rmdir'
-
-alias cl='clear'
