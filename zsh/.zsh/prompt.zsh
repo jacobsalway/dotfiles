3843db202bd53a121f79b2662084b1b5a4eb9dee
xorana <7677151+xorana@users.noreply.github.com>
2017-12-19 10:03:19 +1100

    fix bug with remote and commit indicators

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -21,10 +21,10 @@ autoload -Uz vcs_info
 
 zstyle ':vcs_info:*' stagedstr ' staged'
 zstyle ':vcs_info:*' unstagedstr ' unstaged'
+zstyle ':vsc_info:*' get-revision true
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' actionformats '[%b|%a] '
-zstyle ':vcs_info:*' formats \
-  '%F{8}%c%u%m%f [%F{13}%b%f]'
+zstyle ':vcs_info:*' formats '%12.12i %F{8}%c%u%m%f [%F{13}%b%f]'
 zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-st git-stash git-remotebranch
 zstyle ':vcs_info:*' enable git
 
@@ -35,42 +35,34 @@ function +vi-git-untracked() {
     fi
 }
 
-
-function +vi-git-remotebranch() {
-    local remote
+function +vi-git-st() {
+    local ahead behind remote
+    local -a gitstatus
 
     # Are we on a remote-tracking branch?
     remote=${$(git rev-parse --verify ${hook_com[branch]}@{upstream} \
         --symbolic-full-name 2>/dev/null)/refs\/remotes\/}
 
-    # The first test will show a tracking branch whenever there is one. The
-    # second test, however, will only show the remote branch's name if it
-    # differs from the local one.
     if [[ -n ${remote} ]] ; then
-    #if [[ -n ${remote} && ${remote#*/} != ${hook_com[branch]} ]] ; then
+        # for git prior to 1.7
+        # ahead=$(git rev-list origin/${hook_com[branch]}..HEAD | wc -l)
+        ahead=$(git rev-list ${hook_com[branch]}@{upstream}..HEAD 2>/dev/null | wc -l)
+        ahead=$(echo "${ahead}" | tr -d '[:space:]')
+        (( $ahead )) && gitstatus+=( "+${ahead}" )
+
+        # for git prior to 1.7
+        # behind=$(git rev-list HEAD..origin/${hook_com[branch]} | wc -l)
+        behind=$(git rev-list HEAD..${hook_com[branch]}@{upstream} 2>/dev/null | wc -l)
+        behind=$(echo "${behind}" | tr -d '[:space:]')
+        (( $behind )) && gitstatus+=( "-${behind}" )
+
         hook_com[branch]="${hook_com[branch]} ${remote}"
+
+        if ! [[ -z ${gitstatus} ]]; then
+            hook_com[branch]+=" ${(j:/:)gitstatus}"
+        fi
     fi
-}
-
-function +vi-git-st() {
-    local ahead behind
-    local -a gitstatus
-
-    # for git prior to 1.7
-    # ahead=$(git rev-list origin/${hook_com[branch]}..HEAD | wc -l)
-    ahead=$(git rev-list ${hook_com[branch]}@{upstream}..HEAD 2>/dev/null | wc -l)
-    ahead=$(echo "${ahead}" | tr -d '[:space:]')
-    (( $ahead )) && gitstatus+=( "+${ahead}" )
-
-    # for git prior to 1.7
-    # behind=$(git rev-list HEAD..origin/${hook_com[branch]} | wc -l)
-    behind=$(git rev-list HEAD..${hook_com[branch]}@{upstream} 2>/dev/null | wc -l)
-    behind=$(echo "${behind}" | tr -d '[:space:]')
-    (( $behind )) && gitstatus+=( "-${behind}" )
     
-    if ! [[ -z ${gitstatus} ]]; then
-        hook_com[branch]+=" ${(j:/:)gitstatus}"
-    fi
 }
 
 # Show count of stashed changes
