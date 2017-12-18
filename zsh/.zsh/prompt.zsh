693ad9fee2f91e3fb0f4a0543c4d3ad67cd74a92
xorana <7677151+xorana@users.noreply.github.com>
2017-12-18 23:38:16 +1100

    WIP prompt changes

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -1,17 +1,40 @@
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
 setopt prompt_subst
+
+function git_prompt_info {
+    git branch 2>/dev/null | awk '/^\*/ { print $2 }'
+}
+
+function git_dirty {
+    local FLAGS
+    FLAGS=('--porcelain')
+    local STATUS
+    STATUS=$(command git status ${FLAGS} 2> /dev/null | tail -n1)
+
+    if [[ -n $STATUS ]]; then
+        echo "*"
+    fi
+}
 
 autoload -Uz vcs_info
 
+zstyle ':vcs_info:*' stagedstr 'staged '
+zstyle ':vcs_info:*' unstagedstr 'unstaged '
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' actionformats '[%b|%a] '
+zstyle ':vcs_info:*' formats \
+  '[%F{13}%b%f] %F{8}%c%u%f%m '
+zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-st git-stash
 zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:git*:*' get-revision true
-zstyle ':vcs_info:git*:*' check-for-changes true
++vi-git-untracked() {
+  if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
+  [[ $(git ls-files --other --directory --exclude-standard | sed q | wc -l | tr -d ' ') == 1 ]] ; then
+  hook_com[unstaged]+='untracked'
+fi
+}
 
-# hash changes branch misc
-zstyle ':vcs_info:git*' formats "(%s) %12.12i %c%u %b%m"
-zstyle ':vcs_info:git*' actionformats "(%s|%a) %12.12i %c%u %b%m"
-
-zstyle ':vcs_info:git*+set-message:*' hooks git-st git-stash
-
+# Show remote ref name and number of commits ahead-of or behind
 function +vi-git-st() {
     local ahead behind remote
     local -a gitstatus
@@ -31,22 +54,27 @@ function +vi-git-st() {
         behind=$(git rev-list HEAD..${hook_com[branch]}@{upstream} 2>/dev/null | wc -l)
         (( $behind )) && gitstatus+=( "${c4}-${behind}${c2}" )
 
-        hook_com[branch]="${hook_com[branch]} [${remote} ${(j:/:)gitstatus}]"
+        if [[ -z "${gitstatus}" ]] ; then
+            hook_com[branch]="${hook_com[branch]} ${remote}"
+        else
+            hook_com[branch]="${hook_com[branch]} ${remote} ${(j:/:)gitstatus}"
+        fi
     fi
 }
 
+# Show count of stashed changes
 function +vi-git-stash() {
     local -a stashes
 
     if [[ -s ${hook_com[base]}/.git/refs/stash ]] ; then
         stashes=$(git stash list 2>/dev/null | wc -l)
-        hook_com[misc]+=" (${stashes} stashed)"
+        hook_com[misc]+="(${stashes} stashed)"
     fi
 }
+
 
 precmd () { vcs_info }
 
 
-PROMPT='%F{8}%D{%H:%M:%S}%f [%F{5}%n%f@%F{5}%M%f] %~ 
+PROMPT='%F{8}%D{%H:%M:%S}%f [%F{5}%n%f@%F{5}%M%f] %~ ${vcs_info_msg_0_}
 $ '
-RPROMPT='${vcs_info_msg_0_}'
