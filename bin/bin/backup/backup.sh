baeaf301b9f4beaa9920a8acabe95a4df33f3e5a
xorana <7677151+xorana@users.noreply.github.com>
2018-05-12 01:35:54 +1000

    Add incremental back up functionality by using rsync --link-dest

--- bin/bin/backup/backup.sh
+++ bin/bin/backup/backup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-TIME="backup-$(date -I)"
+BACKUPDIR=/media/hdd/backups
+NEW="backup-$(date +\%d\%m\%Y_T\%H\%M\%S)"
+OLD=$(ls -1 $BACKUPDIR | sort -t_ -k2.5nr,2 -k2.3nr,2 -k2.1nr,2 -k3r | head -1)
 
-mkdir /media/hdd/backups/$TIME
-sudo rsync -avr -o -g --delete /home/$USER /etc/X11 /etc/systemd /media/hdd/backups/$TIME --exclude Music --exclude .cache
-pacman -Q > /media/hdd/backups/$TIME/packages.txt
+mkdir $BACKUPDIR/$NEW
+
+if [[ -n "$OLD" && -d "$BACKUPDIR/$OLD" ]]; then
+    sudo rsync -av --delete /home/$USER /etc/X11 /etc/systemd /media/hdd/backups/$NEW --exclude Music --exclude .cache --link-dest=$BACKUPDIR/$OLD
+else
+    sudo rsync -av --delete /home/$USER /etc/X11 /etc/systemd /media/hdd/backups/$NEW --exclude Music --exclude .cache
+fi
+
+pacman -Q > /media/hdd/backups/$NEW/packages.txt
