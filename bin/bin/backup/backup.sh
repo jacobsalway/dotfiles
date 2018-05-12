b83de98ea1a29a662b6f79a70712d8e697a20118
xorana <7677151+xorana@users.noreply.github.com>
2018-05-12 13:23:48 +1000

    Change backup script for new folders

--- bin/bin/backup/backup.sh
+++ bin/bin/backup/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BACKUPDIR=/media/hdd/backups
+BACKUPDIR=/media/backup
 NEW="backup-$(date +\%d\%m\%Y_T\%H\%M\%S)"
 OLD=$(ls -1 $BACKUPDIR | sort -t_ -k2.5nr,2 -k2.3nr,2 -k2.1nr,2 -k3r | head -1)
 
@@ -12,4 +12,4 @@ else
     sudo rsync -av --delete /home/$USER /etc/X11 /etc/systemd /media/hdd/backups/$NEW --exclude Music --exclude .cache
 fi
 
-pacman -Q > /media/hdd/backups/$NEW/packages.txt
+pacman -Q > $BACKUPDIR/$NEW/packages.txt
