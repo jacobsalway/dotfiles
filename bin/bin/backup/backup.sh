45fe8d04e240059a929655f4a87244ffe32f2062
xorana <7677151+xorana@users.noreply.github.com>
2018-05-12 19:40:20 +1000

    Modify backup script to back up whole /etc folder

--- bin/bin/backup/backup.sh
+++ bin/bin/backup/backup.sh
@@ -7,9 +7,9 @@ OLD=$(ls -1 $BACKUPDIR | sort -t_ -k2.5nr,2 -k2.3nr,2 -k2.1nr,2 -k3r | head -1)
 mkdir $BACKUPDIR/$NEW
 
 if [[ -n "$OLD" && -d "$BACKUPDIR/$OLD" ]]; then
-    sudo rsync -av --delete /home/$USER /etc/X11 /etc/systemd /media/hdd/backups/$NEW --exclude Music --exclude .cache --link-dest=$BACKUPDIR/$OLD
+    sudo rsync -av --delete /home/$USER /etc $BACKUPDIR/$NEW --exclude Music --exclude .cache --link-dest=$BACKUPDIR/$OLD
 else
-    sudo rsync -av --delete /home/$USER /etc/X11 /etc/systemd /media/hdd/backups/$NEW --exclude Music --exclude .cache
+    sudo rsync -av --delete /home/$USER /etc $BACKUPDIR/$NEW --exclude Music --exclude .cache
 fi
 
 pacman -Q > $BACKUPDIR/$NEW/packages.txt
