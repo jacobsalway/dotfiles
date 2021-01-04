0ae23550e2c09dc641a892a64c02b79f0e4f352b
Jacob Salway <7677151+xorana@users.noreply.github.com>
2021-01-04 15:39:01 +1100

    Remove git prompt

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -1,70 +1,74 @@
 setopt prompt_subst
 
-autoload -Uz vcs_info
+#autoload -Uz vcs_info
 
-zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:*' stagedstr 'S'
-zstyle ':vcs_info:*' unstagedstr 'U'
-zstyle ':vsc_info:git*:*' get-revision true
-zstyle ':vcs_info:git*:*' check-for-changes true
-zstyle ':vcs_info:git*' actionformats '[%b|%a] '
-zstyle ':vcs_info:git*' formats '%F{8}%c%u%m%f [%F{14}%b%f]'
-zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-st git-stash git-remotebranch
+#zstyle ':vcs_info:*' enable git
+#zstyle ':vcs_info:*' stagedstr 'S'
+#zstyle ':vcs_info:*' unstagedstr 'U'
+#zstyle ':vsc_info:git*:*' get-revision true
+#zstyle ':vcs_info:git*:*' check-for-changes true
+#zstyle ':vcs_info:git*' actionformats '[%b|%a] '
+#zstyle ':vcs_info:git*' formats '[%F{14}%b%f] %F{8}%c%u%m%f'
+#zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-st git-stash git-remotebranch
+# zstyle ':vcs_info:git*+set-message:*' hooks git-st git-stash git-remotebranch
 
 # Add indicator for untracked files
-function +vi-git-untracked() {
-    if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
-    [[ $(git ls-files --other --directory --exclude-standard | sed q | wc -l | tr -d ' ') == 1 ]] ; then
-        hook_com[unstaged]+='?'
-    fi
-}
+# function +vi-git-untracked() {
+#    if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
+#    [[ $(git ls-files --other --directory --exclude-standard | sed q | wc -l | tr -d ' ') == 1 ]] ; then
+#        hook_com[unstaged]+='?'
+#    fi
+# }
 
 # Get remote branch name and commit count difference
 function +vi-git-st() {
-    local ahead behind remote
-    local -a gitstatus
+   local ahead behind remote
+   local -a gitstatus
 
-    # Are we on a remote-tracking branch?
-    remote=${$(git rev-parse --verify ${hook_com[branch]}@{upstream} \
-        --symbolic-full-name 2>/dev/null)/refs\/remotes\/}
+   # Are we on a remote-tracking branch?
+   remote=${$(git rev-parse --verify ${hook_com[branch]}@{upstream} \
+       --symbolic-full-name 2>/dev/null)/refs\/remotes\/}
 
-    if [[ -n ${remote} ]] ; then
-        # for git prior to 1.7
-        # ahead=$(git rev-list origin/${hook_com[branch]}..HEAD | wc -l)
-        ahead=$(git rev-list ${hook_com[branch]}@{upstream}..HEAD 2>/dev/null | wc -l)
-        ahead=$(echo "${ahead}" | tr -d '[:space:]')
-        (( $ahead )) && gitstatus+=( "+${ahead}" )
+   if [[ -n ${remote} ]] ; then
+       # for git prior to 1.7
+       # ahead=$(git rev-list origin/${hook_com[branch]}..HEAD | wc -l)
+       ahead=$(git rev-list ${hook_com[branch]}@{upstream}..HEAD 2>/dev/null | wc -l)
+       ahead=$(echo "${ahead}" | tr -d '[:space:]')
+       (( $ahead )) && gitstatus+=( "+${ahead}" )
 
-        # for git prior to 1.7
-        # behind=$(git rev-list HEAD..origin/${hook_com[branch]} | wc -l)
-        behind=$(git rev-list HEAD..${hook_com[branch]}@{upstream} 2>/dev/null | wc -l)
-        behind=$(echo "${behind}" | tr -d '[:space:]')
-        (( $behind )) && gitstatus+=( "-${behind}" )
+       # for git prior to 1.7
+       # behind=$(git rev-list HEAD..origin/${hook_com[branch]} | wc -l)
+       behind=$(git rev-list HEAD..${hook_com[branch]}@{upstream} 2>/dev/null | wc -l)
+       behind=$(echo "${behind}" | tr -d '[:space:]')
+       (( $behind )) && gitstatus+=( "-${behind}" )
 
-        hook_com[branch]="${hook_com[branch]} ${remote}"
+       hook_com[branch]="${hook_com[branch]} ${remote}"
 
-        if ! [[ -z ${gitstatus} ]]; then
-            hook_com[branch]+=" ${(j:/:)gitstatus}"
-        fi
-    fi
-    
+       if ! [[ -z ${gitstatus} ]]; then
+           hook_com[branch]+=" ${(j:/:)gitstatus}"
+       fi
+   fi
+   
 }
 
 # Show count of stashed changes
 function +vi-git-stash() {
-    local -a stashes
+   local -a stashes
 
-    if [[ -s ${hook_com[base]}/.git/refs/stash ]] ; then
-        stashes=$(git stash list 2>/dev/null | wc -l | tr -d '[:space:]')
-        hook_com[misc]+=" (${stashes} stashed)"
-    fi
+   if [[ -s ${hook_com[base]}/.git/refs/stash ]] ; then
+       stashes=$(git stash list 2>/dev/null | wc -l | tr -d '[:space:]')
+       hook_com[misc]+=" (${stashes} stashed)"
+   fi
 }
 
-precmd () { vcs_info }
+# precmd () { vcs_info }
 
 
-PROMPT='[%F{14}%n%f@%F{14}%M%f] %~ 
-$ '
+#PROMPT='%F{14}%n%f@%F{14}%M%f %~ ${vcs_info_msg_0_}
+#❯ '
+
+PROMPT='%F{14}%n%f@%F{14}%M%f %~
+❯ '
 
 # git prompt
-RPROMPT='${vcs_info_msg_0_}'
+RPROMPT=''
