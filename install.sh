ab818a84762f5a532333173f190c349daf587bfd
Jacob Salway <jacob.salway@gmail.com>
2023-12-29 12:50:39 +1100

    Add more config

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ create_link() {
     local source_file=$1
     local destination_file=$2
 
+    # ln fails if the full directory path doesn't exist
+    mkdir -p $(dirname $2)
+
     if [ ! -f "$destination_file" ]; then
         ln -s "$source_file" "$destination_file"
         echo "Linked $destination_file"
@@ -13,5 +16,5 @@ create_link() {
 }
 
 create_link "$(pwd)/vimrc" "$HOME/.vimrc"
-create_link "$(pwd)/vimrc" "$HOME/.config/nvim/init.vim"
+create_link "$(pwd)/init.vim" "$HOME/.config/nvim/init.vim"
 create_link "$(pwd)/zshrc" "$HOME/.zshrc"
--- vimrc
+++ vimrc
@@ -1,1 +1,26 @@
 set nocompatible
+
+" indentation
+set expandtab
+set shiftwidth=2
+set smartindent
+set tabstop=2
+set softtabstop=2
+
+" statusline
+set laststatus=3
+set noshowmode
+
+" line numbers
+set number
+set relativenumber
+
+" colorscheme
+syntax on
+
+" make background transparent
+hi Normal guibg=NONE ctermbg=NONE
+hi LineNr guibg=NONE ctermbg=NONE
+
+" disable splash screen
+set shortmess=I
